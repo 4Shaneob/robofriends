ef520d98541436fd47c7584792b14e3f9b261c16
4Shaneob <4Shane@eircom.net>
2018-04-24 00:55:55 +0100

    Full project after build

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Bob Barker',
+	username: 'Bob',
+	email: 'Bob@Bob.com'
+},
+{
+	id: 2,
+	name: 'John Jonston',
+	username: 'Johnny',
+	email: 'JJ@Jonston.com'
+},
+{
+	id: 3,
+	name: 'David Davidson',
+	username: 'David',
+	email: 'Davey@Davidson.com'
+},
+{
+	id: 4,
+	name: 'Bruce Wayne',
+	username: 'Bruce',
+	email: 'BWayne@Wayneenterprises.com'
+},
+{
+	id: 5,
+	name: 'Richard Grasyson',
+	username: 'Dick',
+	email: 'RGrason@Wayneenterprises.com'
+},
+{
+	id: 6,
+	name: 'Barry Allen',
+	username: 'Flash',
+	email: 'BAllen@Starlabs.com'
+},
+{
+	id: 7,
+	name: 'Oliver Queen',
+	username: 'Ollie',
+	email: 'OQueen@Queeninc.com'
+},
+{
+	id: 8,
+	name: 'John Wick',
+	username: 'John',
+	email: 'Wick@John.com'
+},
+{
+	id: 9,
+	name: 'William',
+	username: 'Bill',
+	email: 'Bill@wildstallions.com'
+},
+{
+	id: 10,
+	name: 'Thedore',
+	username: 'Ted',
+	email: 'Ted@wildstallions.com'
+}
+]
